7f3907611eb8e7aa35111e53eb23caf2888b6ddf
Webtas <72016401+06ergin06@users.noreply.github.com>
2021-02-16 19:16:31 +0300

    Add project files.

---
+++ runningTasks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+namespace runningTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] Memory = Process.GetProcesses();
+
+            foreach (Process prc in Memory)
+            {
+                Console.WriteLine("İşlem: {0}", prc.ProcessName);
+            }
+        }
+    }
+}
